68393fa4ea1822158ba7c6fcc24ae750ca487d65
VMAzure <hello@azureautomotive.it>
2025-03-02 23:34:15 +0100

    css canvas

--- CIG/obj/Debug/net8.0/CIG.AssemblyInfo.cs
+++ CIG/obj/Debug/net8.0/CIG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CIG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e12fdbe18329989d2fd53385659fa95170e35a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0888a450ef98341432dd2826f42871cccac69ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("CIG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CIG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
