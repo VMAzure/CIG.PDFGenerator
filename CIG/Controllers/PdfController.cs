c80a25b2e6dbb7acc2c1364e9ee0d80109657a72
VMAzure <hello@azureautomotive.it>
2025-03-08 10:59:24 +0100

    pdf pag 2 ok mistral imposto le immagini 2

--- CIG/Controllers/PdfController.cs
+++ CIG/Controllers/PdfController.cs
@@ -237,7 +237,7 @@ namespace CIG.PDFGenerator.Controllers
                         }
                     });
                 });
-                });
+               
             });
         }
     }
