8810e1ecbefd710cba6bdbbada9682e1fd69bd78
VMAzure <hello@azureautomotive.it>
2025-03-13 18:19:02 +0100

    salviamo il pdf in supabase

---
+++ CIG/wwwroot/js/supabaseClient.js
@@ -0,0 +1,9 @@
+﻿import { createClient } from '@supabase/supabase-js';
+
+
+const supabaseUrl = 'https://vqfloobaovtdtcuflqeu.supabase.co';
+const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxZmxvb2Jhb3Z0ZHRjdWZscWV1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczOTUzOTUzMCwiZXhwIjoyMDU1MTE1NTMwfQ.Lq-uIgXYZiBJK4ChfF_D7i5qYBDuxMfL2jY5GGKDuVk';
+
+export const supabase = createClient(supabaseUrl, supabaseServiceKey, {
+    auth: { persistSession: false }
+});
---
+++ CIG/wwwroot/js/testUpload.js
@@ -0,0 +1,19 @@
+﻿import { supabase } from './supabaseClient.js';
+
+async function testUploadPdf() {
+    const fileContent = new Blob(["Test upload Supabase"], { type: "application/pdf" });
+    const fileName = "test_upload.pdf";
+
+    const { data, error } = await supabase
+        .storage
+        .from('nlt-preventivi')
+        .upload(fileName, fileContent);
+
+    if (error) {
+        console.error("Errore upload file:", error);
+    } else {
+        console.log("Upload riuscito. Risposta:", data);
+    }
+}
+
+testUploadPdf();
